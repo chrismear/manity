605cc5f8481687aeee930577ab06ac89de2e11c5
Chris Mear <chris@feedmechocolate.com>
2010-03-08 20:36:57 +0000

    FLICK-HER.

--- manity.js
+++ manity.js
@@ -17,15 +17,38 @@
 */
 
 $(document).ready(function () {
-  for (i=0; i<10; i++) {
+  MANITY.bbqs = [];
+  for (i=0; i<7; i++) {
     $('#MANTAGE').append('<div class="PINT"></div>')
   }
   $('.PINT').each(function (i, pint) {
     $(pint).css('margin-left', -(Math.floor(Math.random()*300)) + 'px');
-    for (i=0; i<10; i++) {
-      $(pint).append('<div class="BBQ"></div>')
+    for (j=0; j<7; j++) {
+      bbq = $('<div class="BBQ"></div>');
+      $(pint).append(bbq);
+      MANITY.bbqs.push(bbq);
     }
     $(pint).append('<div class="OLD-SPICE"></div>')
     $(pint).children('.BBQ').height((Math.floor(Math.random() * 11)*5 + 55) + 'px');
+  });
+  
+  // http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json
+  tags = MANITY.HEWORDS.join(',');
+  url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + MANITY.flickrApiKey + "&format=json&media=photos&sort=interestingness-desc&tags=" + escape(tags) + "&jsoncallback=?";
+  $.getJSON(url, function (data) {
+    if (data.stat == "ok") {
+      MANITY.photos = [];
+      $.each(data.photos.photo, function (photoIndex, photo) {
+        MANITY.photos.push("http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg");
+      });
+      photoIndex = 0;
+      $.each(MANITY.bbqs, function (bbqIndex, bbq) {
+        if (photoIndex >= MANITY.photos.length) {
+          photoIndex = 0;
+        };
+        $(bbq).css('background-image', 'url(' + MANITY.photos[photoIndex] + ')');
+        photoIndex++;
+      });
+    }
   });
 });
